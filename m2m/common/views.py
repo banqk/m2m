73ed6601c630cd95eee5f99978f56584b83593b7
fivestarwy <fivestarwy@hotmail.com>
2016-08-08 10:32:45 +0800

    Update views.py

--- m2m/common/views.py
+++ m2m/common/views.py
@@ -24,11 +24,12 @@ def account(request):
         hedge_accounts = Hedge_Account.objects.filter(m2m_account=account_id)
     except Hedge_Account.DoesNotExist:
         hedge_accounts = {}
-    
+    products = Product.objects.filter(m2m_account=account_id)
 #    options.update({'current_account_name': account.name})
     options.update({'account': account})
     options.update({'inventories': inventories})
     options.update({'hedge_accounts': hedge_accounts})
+    options.update({'products': products})
     render_to_url = 'hidden/single_account.html'
     return render_to_response(render_to_url, options)
 def user(request):
